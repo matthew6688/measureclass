1e7b3f9709746c9cca8e8bbf14287a71ad838c58
matthew6688 <matthew6688@gmail.com>
2017-07-02 15:30:20 +1000

    Site updated: 2017-07-02 15:30:20

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Jul 02 2017 15:14:59 GMT+1000 (AEST)
+// build time:Sun Jul 02 2017 15:30:11 GMT+1000 (AEST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
