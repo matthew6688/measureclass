59938b8aee7f16bfa2c935a88efead69827a3ab5
matthew6688 <matthew6688@gmail.com>
2017-07-03 11:49:31 +1000

    Site updated: 2017-07-03 11:49:31

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 03 2017 11:46:42 GMT+1000 (AEST)
+// build time:Mon Jul 03 2017 11:49:22 GMT+1000 (AEST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
